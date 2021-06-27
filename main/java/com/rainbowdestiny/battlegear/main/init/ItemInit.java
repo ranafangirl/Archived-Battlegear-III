b450887282f0345674006c51c3cd5f2ab8d262de
Bug1312 <59811382+Bug1312@users.noreply.github.com>
2021-06-27 13:26:02 -0400

    added other axes ( No textures )

--- main/java/com/rainbowdestiny/battlegear/main/init/ItemInit.java
+++ main/java/com/rainbowdestiny/battlegear/main/init/ItemInit.java
@@ -17,7 +17,12 @@ public class ItemInit {
 	public static final RegistryObject<Item> HERELDRY = ITEMS.register("hereldry", () -> new Item (new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP)));
 	
 	//War Axe
+	public static final RegistryObject<Item> WOOD_WAR_AXE = ITEMS.register("wood_war_axe", () -> new WarAxeItem(ItemTier.WOOD, 3, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
+	public static final RegistryObject<Item> STONE_WAR_AXE = ITEMS.register("stone_war_axe", () -> new WarAxeItem(ItemTier.STONE, 4, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
 	public static final RegistryObject<Item> IRON_WAR_AXE = ITEMS.register("iron_war_axe", () -> new WarAxeItem(ItemTier.IRON, 5, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
+	public static final RegistryObject<Item> GOLD_WAR_AXE = ITEMS.register("gold_war_axe", () -> new WarAxeItem(ItemTier.GOLD, 3, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
+	public static final RegistryObject<Item> DIAMOND_WAR_AXE = ITEMS.register("diamond_war_axe", () -> new WarAxeItem(ItemTier.DIAMOND, 6, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
+	public static final RegistryObject<Item> NETHERITE_WAR_AXE = ITEMS.register("netherite_war_axe", () -> new WarAxeItem(ItemTier.NETHERITE, 7, 4, new Item.Properties().tab(Battlegear.BATTLEGEAR_GROUP), "A powerful weapon", 0));
 
 	//Shields
 	//public static final RegistryObject<Item> IRON_SHIELD = ITEMS.register("iron_shield", () -> new NewShieldItem(ItemGroup.TAB_COMBAT))));
